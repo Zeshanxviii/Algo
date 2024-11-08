ab9774771bf454cd8546cfe3a4929ea531959bc3
zeeshan <mzh881061@gmail.com>
2024-11-08 23:44:10 +0530

    Fri 8 Nov  11 PM

--- Arrays/rotateArraybyDindex.cpp
+++ Arrays/rotateArraybyDindex.cpp
@@ -4,10 +4,35 @@ using namespace std;
 
 int main()
 {
-    int arr[5] = {2,1,3,4,6};
-    int k = 3;
-    int temp = arr[k];
+    int n = 7;
+    int arr[n] = {1,2,9,2,3,4,5};
+    //for largest K 
+    int k = 3 % n;
+    int temp[k];
+    for(int i=0; i<k; i++)
+    {
+        temp[i] = arr[i];
+    }
+ 
+    //shifting 
 
-    for(int i=k;)
+    for(int i=k; i<n; i++)
+    {
+        arr[i-k] = arr[i];
+    }
+    
+    //putting back rest of the element from temp
+    int j = 0;
+    for(int i=n-k; i<n; i++)
+    {
+        arr[i] = temp[j];
+        j++;
+    }
+    //print array 
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
