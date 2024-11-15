bd3dfc22c713a7c16bc571cf626715e977f31795
zeeshan <mzh881061@gmail.com>
2024-11-15 13:04:23 +0530

    Fri Nov 15

--- Arrays/majorityElement.cpp
+++ Arrays/majorityElement.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
+//brute force approch
+/*int main()
 {
     int arr[5] = {1 , 2 , 1 , 5 , 1};
     for( int i = 0; i < 5; i++)
@@ -22,5 +23,92 @@ int main()
         }
 
     }
+    return 0;
+}*/
+
+
+
+//better approch
+/*
+int main()
+{
+    vector<int> arr = {1,2,1,2,3,1,1};
+    int n = arr.size();
+    map<int,int>mapp;
+
+    for(int i=0; i<n; i++)
+    {
+        mapp[arr[i]]++;
+    }
+
+    for( auto it:mapp )
+    {
+        if(it.second > n/2)
+        {
+            cout<<it.first<<endl;
+        }
+
+    }
+
+    cout<<endl;
+
+    return 0;
+}*/
+
+
+//optimize solution using hashmap;
+int findcandidateElement(vector<int> &nums)
+{
+    int max = 0, cnt = 1;
+
+    for(int i=1; i<nums.size(); i++)
+    {
+        if(nums[max]==nums[i])
+        {
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+        if(cnt==0)
+        {
+            max = i;
+            cnt = 1;
+        }
+    }
+
+    return nums[max];
+}
+
+bool ismajority(vector<int > &nums,int cand)
+{
+    int count = 0;
+    for(int i=0; i<nums.size(); i++)
+    {
+        if(nums[i]==cand)
+        {
+            count++;
+        }
+    }
+
+    if(count > nums.size()/2) 
+    return 1;
+    else  
+    return 0;
+}
+
+int main()
+{
+    vector<int> nums = {2,2,1,1,1,2,2};
+    int cand = findcandidateElement(nums);
+    cout<<cand;
+    if(ismajority(nums,cand))
+    {
+        cout<<cand<<endl;
+    }
+    else{
+        cout<<-1;
+    }
+
     return 0;
 }
