2e53d72277fd0ea04ee4e82fa5582b9d3bd92a71
zeeshan <mzh881061@gmail.com>
2024-11-22 23:34:53 +0530

    Nov 22

---
+++ Arrays/setMatrix.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void setZeroes(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    // Flags to check if the first row and first column should be zeroed
+    bool firstRowZero = false;
+    bool firstColZero = false;
+
+    // Check if the first row has any zero
+    for (int j = 0; j < n; j++) {
+        if (matrix[0][j] == 0) {
+            firstRowZero = true;
+            break;
+        }
+    }
+
+    // Check if the first column has any zero
+    for (int i = 0; i < m; i++) {
+        if (matrix[i][0] == 0) {
+            firstColZero = true;
+            break;
+        }
+    }
+
+    // Use first row and first column to mark zeroes
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;  // Mark row
+                matrix[0][j] = 0;  // Mark column
+            }
+        }
+    }
+
+    // Set cells to zero based on the markers in the first row and column
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Set the first row to zero if necessary
+    if (firstRowZero) {
+        for (int j = 0; j < n; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Set the first column to zero if necessary
+    if (firstColZero) {
+        for (int i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 0, 6},
+        {7, 8, 9}
+    };
+
+    // Calling the function to set zeroes
+    setZeroes(matrix);
+
+    // Output the modified matrix
+    cout << "Modified Matrix:" << endl;
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
